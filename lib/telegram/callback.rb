71b5b598e20c77053f227f7b712517b18bbbc8d1
ssut <ssut@ssut.me>
2015-07-06 18:36:45 +0900

    Update: documentation for Callback class

--- lib/telegram/callback.rb
+++ lib/telegram/callback.rb
@@ -1,5 +1,11 @@
 module Telegram
+  # Callback class for em-synchrony
+  #
+  # @note You don't need to make this callback object without when it needed
+  # @see API
+  # @version 0.1.0
   class Callback
+    # @return [Object] Data
     attr_reader :data
 
     def initialize
@@ -8,14 +14,24 @@ module Telegram
       @data = nil
     end
 
+    # Set a callback to be called when succeed
+    #
+    # @param [Block] cb
     def callback(&cb)
       @success = cb
     end
 
+    # Set a callback to be called when failed
+    #
+    # @param [Block] cb
     def errback(&cb)
       @fail = cb
     end
 
+    # Trigger either success or error actions with data
+    # 
+    # @param [Symbol] type :success or :fail
+    # @param [Object] data
     def trigger(type = :success, data = nil)
       @data = data
       case type
